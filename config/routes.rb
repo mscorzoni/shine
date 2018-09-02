7c7789bb7ce5e8bc2343ed1b200e67431a9b339e
Marcos Scorzoni <mlessa@gmail.com>
2018-09-02 11:48:50 +0100

    add customers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers_controller/index'
   devise_for :users
   root to: 'dashboard#index'
+  resources :customers, only: [:index]
 end
